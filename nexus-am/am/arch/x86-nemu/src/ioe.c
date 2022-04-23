657f4f202e4c0b91b7edff02ac7bd049996ae626
tracer-ics2017 <tracer@njuics.org>
2022-04-23 14:06:07 +0800

    >  run 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  14:06:07 up  2:09,  2 users,  load average: 0.00, 0.33, 0.51 b1bcb6321b4d43c1a83e2c86aa100bb566593c2c

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -1,46 +1,51 @@
 #include <am.h>
 #include <x86.h>
 
-#define RTC_PORT 0x48   // Note that this is not standard
+#define RTC_PORT 0x48 // Note that this is not standard
 static unsigned long boot_time;
 
-void _ioe_init() {
+void _ioe_init()
+{
   boot_time = inl(RTC_PORT);
 }
 
-unsigned long _uptime() {
+unsigned long _uptime()
+{
   unsigned long ms = inl(RTC_PORT) - boot_time;
   return ms;
 }
 
-uint32_t* const fb = (uint32_t *)0x40000;
+uint32_t *const fb = (uint32_t *)0x40000;
 
 _Screen _screen = {
-  .width  = 400,
-  .height = 300,
+    .width = 400,
+    .height = 300,
 };
 
-extern void* memcpy(void *, const void *, int);
+extern void *memcpy(void *, const void *, int);
 
-void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
-  int i;
-  for (i = 0; i < _screen.width * _screen.height; i++) 
-    fb[i] = i;
-
+void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h)
+{
   /*
+  int i;
+  for (i = 0; i < _screen.width * _screen.height; i++)
+    fb[i] = i;
+*/
   int temp = (w > _screen.width - x) ? _screen.width - x : w;
-  int cp_bytes = size_of(uint32_t)* temp;
-  for(int j = 0; j < h && y + j < _screen.height; j++) {
+  int cp_bytes = 4 * temp;
+  for (int j = 0; j < h && y + j < _screen.height; j++)
+  {
     memcpy(&fb[(y + j) * _screen.width + x], pixels, cp_bytes);
     pixels += w;
-  }*/
+  }
+}
+void _draw_sync()
+{
 }
 
-void _draw_sync() {
-}
-
-int _read_key() {
-  if(inb(0x64))
+int _read_key()
+{
+  if (inb(0x64))
     return inl(0x60);
   else
     return _KEY_NONE;
