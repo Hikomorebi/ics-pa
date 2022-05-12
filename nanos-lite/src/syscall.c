7657ecd2c3f955aea474669122814210c011f353
tracer-ics2017 <tracer@njuics.org>
2022-05-12 22:16:49 +0800

    >  run 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:16:49 up  6:44,  2 users,  load average: 0.00, 0.00, 0.00 fb78bcf426a30273533bd85d31b5356f1cd238dd

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,39 @@
 #include "common.h"
-#include "syscall.h"
-
+enum {
+  SYS_none,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_exit,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
+//#include "syscall.h"
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
+
 
   switch (a[0]) {
+    case SYS_none:
+      SYSCALL_ARG1(r) = 1;
+      break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
