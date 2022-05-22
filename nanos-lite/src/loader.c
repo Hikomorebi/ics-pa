d95aa06fa01f47965d7358c63a94598605bcfa29
tracer-ics2017 <tracer@njuics.org>
2022-05-22 12:54:50 +0800

    >  run 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  12:54:50 up  2:48,  2 users,  load average: 1.72, 1.37, 0.97 ac1d64b837324c4f76d83aec397ea808f98995f8

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,7 +1,7 @@
 #include "common.h"
 #include "fs.h"
 
-#define DEFAULT_ENTRY ((void *)0x4000000)
+#define DEFAULT_ENTRY ((void *)0x8048000)
 extern size_t get_ramdisk_size();
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 uintptr_t loader(_Protect *as, const char *filename) {
