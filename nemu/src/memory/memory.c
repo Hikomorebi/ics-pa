329c63b7e8de27da9440416249a6a0cddfd726fe
tracer-ics2017 <tracer@njuics.org>
2022-05-22 12:35:15 +0800

    >  compile 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  12:35:15 up  2:29,  2 users,  load average: 0.26, 0.07, 0.02 796cfa8386cb7e4eaae46d35e9e963a668fc992f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -39,7 +39,16 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   if( PTE_ADDR(addr) != PTE_ADDR(addr + len -1)) {
-    assert(0);
+    int num1 = 0x1000 - OFF(addr);
+    int num2 = len - num1;
+    paddr_t paddr1 = page_translate(addr,false);
+    paddr_t paddr2 = page_translate(addr+num1,false);
+
+    uint32_t low = paddr_read(paddr1,num1);
+    uint32_t high = paddr_read(paddr2,num2);
+
+    uint32_t result = high << (num1 * 8) | low;
+    return result;
   }
   else {
     paddr_t paddr = page_translate(addr,false);
@@ -49,7 +58,17 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
   if( PTE_ADDR(addr) != PTE_ADDR(addr + len -1)) {
-    assert(0);
+    int num1 = 0x1000 - OFF(addr);
+    int num2 = len - num1;
+    paddr_t paddr1 = page_translate(addr,true);
+    paddr_t paddr2 = page_translate(addr+num1,true);
+
+    uint32_t low = data & (~0u >> ((4 - num1) << 3));
+    uint32_t high = data >> ((4 - num2) << 3);
+
+    paddr_write(paddr1,num1,low);
+    paddr_write(paddr2,num2,high);
+    return;
   }
   else {
     paddr_t paddr = page_translate(addr,true);
