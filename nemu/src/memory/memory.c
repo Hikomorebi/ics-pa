686b4d4277d0f9438d01af486367968812e7dc67
tracer-ics2017 <tracer@njuics.org>
2022-05-22 11:15:38 +0800

    >  compile 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:15:38 up  1:09,  2 users,  load average: 0.01, 0.02, 0.00 d1810d61733c14d4f42a2af71ec73df36aa7d72

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
 #include "device/mmio.h"
+#include "memory/mmu.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -8,6 +9,13 @@
     guest_to_host(addr); \
     })
 
+#define PTE_ADDR(pte) ((uint32_t)(pte) & ~0xfff)
+#define PDX(va) (((uint32_t)(va) >> 22) & 0x3ff)
+#define PTX(va) (((uint32_t)(va) >> 12) & 0x3ff)
+#define OFF(va) ((uint32_t)(va) & 0xfff)
+
+
+paddr_t page_translate(vaddr_t addr, bool iswrite);
 uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
@@ -30,9 +38,46 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if( PTE_ADDR(addr) != PTE_ADDR(addr + len -1)) {
+    assert(0);
+  }
+  else {
+    paddr_t paddr = page_translate(addr,false);
+    return paddr_read(paddr,len);
+  }
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  if( PTE_ADDR(addr) != PTE_ADDR(addr + len -1)) {
+    assert(0);
+  }
+  else {
+    paddr_t paddr = page_translate(addr,true);
+    paddr_write(paddr,len,data);
+  } 
+}
+
+paddr_t page_translate(vaddr_t addr, bool iswrite) {
+  CR0 cr_0 = (CR0)cpu.cr0;
+  if(cr_0.paging && cr_0.protect_enable) {  //处于保护模式并开启分页机制
+    CR3 cr_3 = (CR3)cpu.cr3;
+
+    PDE* pgdirs = (PDE*)PTE_ADDR(cr_3.val); //从CR3中获取页目录表的基地址
+    PDE pde = (PDE)paddr_read((uint32_t)(pgdirs + PDX(addr)),4);
+    Assert(pde.present,"addr=0x%x",addr);
+
+    PTE* ptab = (PTE*)PTE_ADDR(pde.val);
+    PTE pte = (PTE)paddr_read((uint32_t)(ptab + PTX(addr)),4);
+    Assert(pte.present,"addr=0x%x",addr);
+
+    pde.accessed = 1;
+    pte.accessed = 1;
+    if(iswrite)
+      pte.dirty = 1;
+    
+    paddr_t paddr = PTE_ADDR(pte.val) | OFF(addr);
+    printf("vaddr=0x%x\tpaddr=0x%x\n",addr,paddr);
+    return paddr;
+  }
+  return addr;
 }
