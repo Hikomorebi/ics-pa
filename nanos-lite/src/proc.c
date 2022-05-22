9eb35938c47cb32079bf2f4612065f920792fff1
tracer-ics2017 <tracer@njuics.org>
2022-05-22 13:53:49 +0800

    >  run 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  13:53:49 up  3:47,  2 users,  load average: 0.41, 0.33, 0.55 99c62a2831cc52dd4e519d71f56cdbb85fa28b32

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -13,18 +13,18 @@ void load_prog(const char *filename) {
   _protect(&pcb[i].as);
 
   uintptr_t entry = loader(&pcb[i].as, filename);
-
+  Log("aa\n\n");
   // TODO: remove the following three lines after you have implemented _umake()
   _switch(&pcb[i].as);
   current = &pcb[i];
   ((void (*)(void))entry)();
-
+  Log("aabb\n\n");
   _Area stack;
   stack.start = pcb[i].stack;
   stack.end = stack.start + sizeof(pcb[i].stack);
-
+Log("aabbv\n\n");
   pcb[i].tf = _umake(&pcb[i].as, stack, stack, (void *)entry, NULL, NULL);
-    Log("aa\n\n");
+
 }
 
 _RegSet* schedule(_RegSet *prev) {
