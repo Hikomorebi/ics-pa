bc6be383a07ed90868338befafdefa27d8ee6300
tracer-ics2017 <tracer@njuics.org>
2022-05-22 11:16:16 +0800

    >  compile 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:16:16 up  1:10,  2 users,  load average: 0.16, 0.05, 0.01 74fd6b9c3ad58a1dc1396164c8b9ff18898e8268

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -76,7 +76,7 @@ paddr_t page_translate(vaddr_t addr, bool iswrite) {
       pte.dirty = 1;
     
     paddr_t paddr = PTE_ADDR(pte.val) | OFF(addr);
-    printf("vaddr=0x%x\tpaddr=0x%x\n",addr,paddr);
+    printf("vaddr=0x%08x\tpaddr=0x%08x\n",addr,paddr);
     return paddr;
   }
   return addr;
