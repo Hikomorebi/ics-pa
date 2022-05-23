07b8e329296016e816014e0898aaaac1da8672f0
tracer-ics2017 <tracer@njuics.org>
2022-05-23 21:15:59 +0800

    >  run 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:15:59 up  1:12,  2 users,  load average: 0.11, 0.04, 0.01 b46bd2a081a5bdae9492f6a5c72a3bb15ec65932

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,6 +4,9 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL:
       return do_syscall(r);
+    case _EVENT_TRAP:
+      printf("event:self-trapped\n");
+      return NULL;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,8 @@ int main() {
 
   //uint32_t entry = loader(NULL, "/bin/hello");
   //((void (*)(void))entry)();
-  load_prog("/bin/pal");
+  load_prog("/bin/dummy");
+  _trap();
 
   panic("Should not reach here");
 }
