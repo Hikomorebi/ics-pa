d74080db95f25cadf9e1d38e4757a5525b61fcb2
tracer-ics2017 <tracer@njuics.org>
2022-05-23 21:34:25 +0800

    >  run 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:34:25 up  1:30,  2 users,  load average: 0.14, 0.03, 0.01 605b8063f8c23fa64428c6ff66718c62d12cd703

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -33,7 +33,7 @@ int main() {
 
   //uint32_t entry = loader(NULL, "/bin/hello");
   //((void (*)(void))entry)();
-  load_prog("/bin/dummy");
+  load_prog("/bin/pal");
   _trap();
 
   panic("Should not reach here");
