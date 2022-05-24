b51c68ffc983ede06b6a11a06b8ca47138f3e001
tracer-ics2017 <tracer@njuics.org>
2022-05-24 12:21:44 +0800

    >  run 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  12:21:44 up  2:54,  2 users,  load average: 0.04, 0.10, 0.04 ee7c897b34d76d3c5353c086207803536102bd41

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -53,6 +53,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 	for (i = 0; i < NR_FILES; i++) {
 		//printf("file name: %s\n", file_table[i].name);
 		if (strcmp(file_table[i].name, pathname) == 0) {
+      set_open_offset(i,0);
 			return i;
 		}
 	}
