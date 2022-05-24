c9483b6aa9c240f9a6f360fff45d1528c8c64435
tracer-ics2017 <tracer@njuics.org>
2022-05-24 11:20:51 +0800

    >  compile 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  11:20:51 up  1:54,  2 users,  load average: 0.06, 0.03, 0.01 493b3244520fb82c6f3828f772cdc6d9c4f619df

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -248,11 +248,12 @@ void exec_wrapper(bool print_flag) {
 #ifdef DIFF_TEST
   uint32_t eip = cpu.eip;
 #endif
+  update_eip();
   if(cpu.INTR & cpu.eflags.IF) {
     cpu.INTR = false;
     raise_intr(TIME_IRQ,cpu.eip);
   }
-  update_eip();
+
 
 
 #ifdef DIFF_TEST
