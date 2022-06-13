2d267c2db9138819b18602831640ea130dcb1c7e
tracer-ics2017 <tracer@njuics.org>
2022-06-13 15:38:35 +0800

    >  run 1911417 komorebi Linux komorebi-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:38:35 up 35 min,  1 user,  load average: 1.15, 1.17, 0.84 b16652c9e5bfd47d2f8b19fc548d4bed949a3fd1

--- navy-apps/apps/pal/src/FLOAT/FLOAT.c
+++ navy-apps/apps/pal/src/FLOAT/FLOAT.c
@@ -2,17 +2,36 @@
 #include <stdint.h>
 #include <assert.h>
 
-FLOAT F_mul_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+FLOAT F_mul_F(FLOAT a, FLOAT b)
+{
+  return ((uint64_t)a * b) >> 16;
 }
 
-FLOAT F_div_F(FLOAT a, FLOAT b) {
-  assert(0);
-  return 0;
+FLOAT F_div_F(FLOAT a, FLOAT b)
+{
+  assert(b != 0);
+  int sign = 1;
+  a = a < 0 ? -a : a;
+  b = b < 0 ? -b : b;
+  if ((a ^ b) & (0 x1 << 31))
+    sign = -1;
+  int res = a / b;
+  a %= b;
+  for (int i = 0; i < 16; i++)
+  {
+    a <<= 1;
+    res <<= 1;
+    i f(a >= b)
+    {
+      a -= b;
+      res++;
+    }
+  }
+  return res * sign;
 }
 
-FLOAT f2F(float a) {
+FLOAT f2F(float a)
+{
   /* You should figure out how to convert `a' into FLOAT without
    * introducing x87 floating point instructions. Else you can
    * not run this code in NEMU before implementing x87 floating
@@ -27,33 +46,38 @@ FLOAT f2F(float a) {
   return 0;
 }
 
-FLOAT Fabs(FLOAT a) {
+FLOAT Fabs(FLOAT a)
+{
   assert(0);
   return 0;
 }
 
 /* Functions below are already implemented */
 
-FLOAT Fsqrt(FLOAT x) {
+FLOAT Fsqrt(FLOAT x)
+{
   FLOAT dt, t = int2F(2);
 
-  do {
+  do
+  {
     dt = F_div_int((F_div_F(x, t) - t), 2);
     t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+  } while (Fabs(dt) > f2F(1e-4));
 
   return t;
 }
 
-FLOAT Fpow(FLOAT x, FLOAT y) {
+FLOAT Fpow(FLOAT x, FLOAT y)
+{
   /* we only compute x^0.333 */
   FLOAT t2, dt, t = int2F(2);
 
-  do {
+  do
+  {
     t2 = F_mul_F(t, t);
     dt = (F_div_F(x, t2) - t) / 3;
     t += dt;
-  } while(Fabs(dt) > f2F(1e-4));
+  } while (Fabs(dt) > f2F(1e-4));
 
   return t;
 }
